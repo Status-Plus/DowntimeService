119e583e01cb2ed7f4a67defc6856631b5e1783a
eswebber07 <40151425+eswebber07@users.noreply.github.com>
2021-01-19 21:36:53 -0500

    yeah let's wait for api updates

--- src/ServerScriptService/MainModule.lua
+++ src/ServerScriptService/MainModule.lua
@@ -1,3 +1,5 @@
+
+
 local DowntimeService = {}
 
 local HttpService = game:GetService("HttpService")
@@ -26,15 +28,39 @@ function DowntimeService.GetSiteStatus()
 		return "Service is Down."
 		
 	end
-	
-	
-	
+
+
+
 	
 end
 
 
 function DowntimeService.GetAvatarAPIStatus()
 	
+	local GetStatus = HttpService:GetAsync("lol this is a placeholder url lmao2")
+	
+	local Data = HttpService:JSONDecode(GetStatus)
+	
+	if Data.placeholderlol == "placeholderpog2" then
+		
+		
+		return "Service is Healthy."
+		
+	elseif Data.placeholderlol == "placeholdermeh2" then 
+		
+		
+		return "Service is Degraded."
+		
+		
+	elseif Data.placeholderlol == "placeholderdown2" then 
+		
+		
+		return "Service is Down."
+		
+	end
+
+
+
 	
 end
 
