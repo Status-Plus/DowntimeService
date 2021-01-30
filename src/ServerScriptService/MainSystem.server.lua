1c8218a07a24459fd2a02d628937353c3c748bdf
eswebber07 <40151425+eswebber07@users.noreply.github.com>
2021-01-30 16:22:11 -0500

    Example File

--- src/ServerScriptService/MainSystem.server.lua
+++ src/ServerScriptService/MainSystem.server.lua
@@ -1,1 +1,49 @@
-local DowntimeService = require(game.ServerScriptService:WaitForChild("MainModule")) -- or require by ID for updates quicker than :lighting:
+local DowntimeService = require(6317978171) -- Or require by using the file path. (Requiring by ID is better for quicker updates.)
+local Players = game:GetService("Players")
+
+
+
+-- Example #1: Kicking Players if Datastores are down.
+
+Players.PlayerAdded:Connect(function(player)
+    
+    
+    if DowntimeService:GetDatastoreAPIStatus() == "down" then
+
+
+        player:Kick("Datastores are currently down. To prevent critical errors we have kicked you. If this issue persits please contact the game owner. =")
+
+    end
+    
+    
+    
+    
+   
+end)
+
+
+
+
+-- Printing latest tests if something is down or not. 
+
+
+while true do
+    
+if DowntimeService:GetSiteStatus() == "down" then
+
+   warn("Roblox Website is Currently Down! Please be patient while Roblox works on a fix! :)") 
+
+elseif DowntimeService.GetSiteStatus() == "degraded" then
+
+    print("Roblox Website is Currently slow! Please be patient while Roblox works on it! :)")
+
+
+elseif DowntimeService:GetSiteStatus() == "up" then
+    
+
+    print("All systems normal!")
+
+end
+
+
+end
