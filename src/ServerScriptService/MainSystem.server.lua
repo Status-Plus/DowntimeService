3cebebf59d5159e068b61ac8ee156ddc0d7d3b59
J-Man775 <jenus@outlook.com>
2022-07-04 18:49:40 +0200

    Update MainSystem.server.lua

--- src/ServerScriptService/MainSystem.server.lua
+++ src/ServerScriptService/MainSystem.server.lua
@@ -1,23 +1,27 @@
 local DowntimeService = require(6317978171) -- Or require by using the file path. (Requiring by ID is better for quicker updates.)
 local Players = game:GetService("Players")
 
--- Example #1: Kicking Players if Datastores are down.
+-- Example #1: Kicking Players if Datastores are down and the variable is set to true.
+
+local kickPlayers = true
 
 Players.PlayerAdded:Connect(function(player)
-	if DowntimeService.statusFromAlias("datastore")== "Down" then
-		player:Kick("Datastores are currently down. To prevent critical errors we have kicked you. If this issue persits please contact the game owner. =")
+	if DowntimeService.statusFromAlias("datastore"):lower() == "down" then
+		if kickPlayers then
+				player:Kick("Datastores are currently down. To prevent critical errors we have kicked you. If this issue persits please contact the game owner. =")
+		end
 	end 
 end)
 
 -- Example #2 Printing the Roblox website's status every 5 minutes. 
 
 while true do
-	local siteStatus = DowntimeService.statusFromAlias("site")
-	if siteStatus == "Down" then
+	local siteStatus = DowntimeService.statusFromAlias("site"):lower()
+	if siteStatus == "down" then
 		warn("Roblox Website is Currently Down! Please be patient while Roblox works on a fix! :)") 
-	elseif siteStatus == "Degraded" then
+	elseif siteStatus == "degraded" then
 		print("Roblox Website is Currently slow! Please be patient while Roblox works on it! :)")
-	elseif siteStatus == "Up" then
+	elseif siteStatus == "up" then
 		print("Roblox website is currently up!")
 	end
 	wait(300)
